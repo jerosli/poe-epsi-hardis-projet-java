6bcf466de30cf59ff1a5c6a821e0a0b9c721f564
Jérôme <jeeronimo@laposte.net>
2017-03-15 15:22:09 +0100

    Lancement du projet cave à vins

---
+++ src/main/java/fr/epsi/poe/hardis/projet_java/CaveAVinsApplication.java
@@ -0,0 +1,12 @@
+package fr.epsi.poe.hardis.projet_java;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CaveAVinsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CaveAVinsApplication.class, args);
+	}
+}
---
+++ src/test/java/fr/epsi/poe/hardis/projet_java/CaveAVinsApplicationTests.java
@@ -0,0 +1,16 @@
+package fr.epsi.poe.hardis.projet_java;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class CaveAVinsApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
