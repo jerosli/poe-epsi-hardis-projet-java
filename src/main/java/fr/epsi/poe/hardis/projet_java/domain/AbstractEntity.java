ef2b668751a7c9ab5d62e265f8fb3ba7c92cae57
Yannis Mazzer <yannis@ioweb.fr>
2017-03-15 15:40:57 +0100

    Created base Entity for uniform Entities definition

---
+++ src/main/java/fr/epsi/poe/hardis/projet_java/domain/AbstractEntity.java
@@ -0,0 +1,57 @@
+package fr.epsi.poe.hardis.projet_java.domain;
+
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.MappedSuperclass;
+
+/**
+ * @author Yannis Mazzer <yannis@ioweb.fr>
+ * @created_on 15/03/2017
+ */
+@MappedSuperclass
+public class AbstractEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    /**
+     * Returns the identifier of the entity.
+     *
+     * @return the id
+     */
+    public Long getId() {
+        return id;
+    }
+
+    /*
+     * (non-Javadoc)
+     * @see java.lang.Object#equals(java.lang.Object)
+     */
+    @Override
+    public boolean equals(Object obj) {
+
+        if (this == obj) {
+            return true;
+        }
+
+        if (this.id == null || obj == null || !(this.getClass().equals(obj.getClass()))) {
+            return false;
+        }
+
+        AbstractEntity that = (AbstractEntity) obj;
+
+        return this.id.equals(that.getId());
+    }
+
+    /*
+     * (non-Javadoc)
+     * @see java.lang.Object#hashCode()
+     */
+    @Override
+    public int hashCode() {
+        return id == null ? 0 : id.hashCode();
+    }
+
+}
