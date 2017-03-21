f926abdd558b2bbe5c4da99e7efab13572fc58f0
Jérôme <jeeronimo@laposte.net>
2017-03-21 15:25:20 +0100

    - refactor 2L à appellation - ajout des annotations sur Vin

---
+++ src/main/java/fr/epsi/poe/hardis/projet_java/domain/Couleur.java
@@ -0,0 +1,11 @@
+package fr.epsi.poe.hardis.projet_java.domain;
+
+/**
+ * Projet : cave_a_vins
+ * Créé par jerome le 21/03/17.
+ */
+public enum Couleur {
+    ROUGE,
+    ROSÉ,
+    BLANC;
+}
---
+++ src/main/java/fr/epsi/poe/hardis/projet_java/domain/Vin.java
@@ -0,0 +1,83 @@
+package fr.epsi.poe.hardis.projet_java.domain;
+
+/**
+ * Projet : cave_a_vins
+ * Créé par jerome le 21/03/17.
+ */
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import java.util.Set;
+
+@Entity
+public class Vin extends AbstractEntity{
+
+    // CHAMPS
+    @Column
+    private String complementNom;
+    @Column
+    private Couleur couleur;
+    @Column
+    private String producteur;
+    @Column
+    private String cru;
+    @Column
+    private Appellation appellation;
+    @Column
+    private int millésime;
+    @Column
+    private Set<Cepage> cépage;
+
+    // CONSTRUCTEURS
+
+    public Vin(Long id, String complementNom, Couleur couleur, String producteur, String cru, int millésime) {
+        this.id = id;
+        this.complementNom = complementNom;
+        this.couleur = couleur;
+        this.producteur = producteur;
+        this.cru = cru;
+        this.millésime = millésime;
+    }
+
+    // ACCESSEURS ET MUTATEURS
+
+    public String getComplementNom() {
+        return complementNom;
+    }
+
+    public void setComplementNom(String complementNom) {
+        this.complementNom = complementNom;
+    }
+
+    public Couleur getCouleur() {
+        return couleur;
+    }
+
+    public void setCouleur(Couleur couleur) {
+        this.couleur = couleur;
+    }
+
+    public String getProducteur() {
+        return producteur;
+    }
+
+    public void setProducteur(String producteur) {
+        this.producteur = producteur;
+    }
+
+    public String getCru() {
+        return cru;
+    }
+
+    public void setCru(String cru) {
+        this.cru = cru;
+    }
+
+    public int getMillésime() {
+        return millésime;
+    }
+
+    public void setMillésime(int millésime) {
+        this.millésime = millésime;
+    }
+}
