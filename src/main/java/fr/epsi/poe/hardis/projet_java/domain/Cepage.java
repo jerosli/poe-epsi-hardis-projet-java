e3dcb248eaa59003fd99262738c5c7962816124f
VALE <borrel.valerie@gmail.com>
2017-03-21 15:12:33 +0100

    COR classe Region et Cepage abstract

---
+++ src/main/java/fr/epsi/poe/hardis/projet_java/domain/Cepage.java
@@ -0,0 +1,24 @@
+package fr.epsi.poe.hardis.projet_java.domain;
+
+import javax.persistence.Entity;
+
+/**
+ * Created by VALE on 21/03/2017.
+ */
+@Entity
+public class Cepage extends AbstractEntity {
+    private String nom;
+
+    public Cepage(long id, String nom) {
+        this.id = id;
+        this.nom = nom;
+    }
+
+   public String getNom() {
+        return nom;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+}
