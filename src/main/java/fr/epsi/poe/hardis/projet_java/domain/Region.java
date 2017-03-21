7c52dc0ad19c658ecd5c3099f64e4180b8bcea1f
VALE <borrel.valerie@gmail.com>
2017-03-21 15:19:24 +0100

    COR classe Region / ajout @Entity

--- src/main/java/fr/epsi/poe/hardis/projet_java/domain/Region.java
+++ src/main/java/fr/epsi/poe/hardis/projet_java/domain/Region.java
@@ -1,23 +1,17 @@
 package fr.epsi.poe.hardis.projet_java.domain;
 
+import javax.persistence.Entity;
+
 /**
  * Created by VALE on 21/03/2017.
  */
-public class Region {
-    private long id;
+@Entity
+public class Region extends AbstractEntity{
     private String nom;
 
     public Region(long id, String nom) {
         this.id = id;
         this.nom = nom;
-    }
-
-    public long getId() {
-        return id;
-    }
-
-    public void setId(long id) {
-        this.id = id;
     }
 
     public String getNom() {
